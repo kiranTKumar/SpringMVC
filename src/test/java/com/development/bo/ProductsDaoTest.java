741a470f85314955fce60e76001cb73ae34a3208
Shankhu Reddy <Shankhu Reddy@LAPTOP-KDGF2SEI>
2018-04-06 17:11:30 -0400

    First commit

---
+++ src/test/java/com/development/bo/ProductsDaoTest.java
@@ -0,0 +1,19 @@
+package com.development.bo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProductsDaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void create() {
+		fail("Not yet implemented");
+	}
+
+}
